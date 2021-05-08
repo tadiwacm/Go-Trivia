95ceda846aafed62ab92a30203b0b3947f88eb91
Theresa <61327598+SudoTheresa@users.noreply.github.com>
2021-05-08 10:29:01 +0200

    levels update

--- Hackaholics/completetasks.js
+++ Hackaholics/completetasks.js
@@ -15,7 +15,32 @@ function taskdone(){
 		alert('You have not completed task 3!');
 		return false;
 	  }
+    else if(!document.getElementById('c4').checked)
+	    {
+		alert('You have not completed task 4!');
+		return false;
+	  }
+    else if(!document.getElementById('c5').checked)
+	    {
+		alert('You have not completed task 5!');
+		return false;
+	  }
+    else if(!document.getElementById('c6').checked)
+	    {
+		alert('You have not completed task 6!');
+		return false;
+	  }
 	else document.getElementById("box").style.display = "block";
   return true;
+}
+
+function warning(){
+	if(document.getElementById('c2').checked){
+		document.getElementById("box2").style.display = "block";
+	}
+}
+
+function closeWin(){
+	box2.close();
 }
 
